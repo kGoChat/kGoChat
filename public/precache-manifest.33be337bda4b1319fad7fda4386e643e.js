f6fb6992a72d74f90b17d58617cc9fde2a23c266
caesar <caesar@kekxv.com>
2020-08-03 16:33:35 +0800

    update WebRTC.js

---
+++ public/precache-manifest.33be337bda4b1319fad7fda4386e643e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "app.js.map?v=8c976bffd6e0df5045c1"
+  },
+  {
+    "url": "app.js?v=8c976bffd6e0df5045c1"
+  },
+  {
+    "url": "chunk-vendors.js.map?v=8c976bffd6e0df5045c1"
+  },
+  {
+    "url": "chunk-vendors.js?v=8c976bffd6e0df5045c1"
+  },
+  {
+    "revision": "a53305eb4caa38a155771957e16b2fc7",
+    "url": "index.html"
+  },
+  {
+    "revision": "a7c72da4e6649ac97c31604bfdb8933d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "static/fonts/element-icons.732389de.ttf"
+  }
+]);
